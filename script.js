b03cb495f960a516c9fadaa3378e5117cf6b084b
lockdownkids67-glitch <lockdownkids67@gmail.com>
2026-06-07 12:34:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function showWelcome() {
+    document.getElementById("welcome").innerHTML =
+    "🚀 Welcome to my portfolio website!";
+}
+
+function showSkills() {
+    document.getElementById("skills").innerHTML =
+    "HTML, CSS, JavaScript and Python";
+}
+
+function showContact() {
+    document.getElementById("contact").innerHTML =
+    "📧 Email: your@email.com";
+}
+
+function showGoal() {
+    alert("My goal is to become a professional web developer!");
+}
